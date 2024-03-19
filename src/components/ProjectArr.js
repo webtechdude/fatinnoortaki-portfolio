659b917d0c10eb20fd9a3af148445f3a207dac89
Web Tech DUDE <71288016+webtechdude@users.noreply.github.com>
2024-03-19 18:18:44 +0000

    Done

--- src/components/ProjectArr.js
+++ src/components/ProjectArr.js
@@ -1,13 +1,14 @@
 export const ProjectArr = [
     {
-        id: 5,
-        name: "Calculator App",
-        description: "Modern Looking Calculator App made using Vanilla Javascript",
-        lang: ["html", "css", "js"],
-        imgUrl: "https://i.ibb.co/5W5TgP7/Screen-Shot-2022-06-05-at-01-41-22.png",
-        repoUrl: "https://github.com/webtechdude/JS-Calculator",
-        url: "https://codepen.io/Web-Tech-DUDE/pen/yLvpemd"
-    }, {
+        id: 1,
+        name: "FitX Final Version v2",
+        description: "Final Version of FitX with PHP",
+        lang: ["html", "css", "scss", "php", "mysql"],
+        imgUrl: "https://i.ibb.co/FmnXLxz/spelaean-mixture-000webhostapp-com-1.png",
+        repoUrl: "https://spelaean-mixture.000webhostapp.com/",
+        url: "https://spelaean-mixture.000webhostapp.com/"
+    },
+    {
         id: 2,
         name: "Javascript Digital Clock",
         description: "Digital Clock Made using Javascript",
@@ -15,7 +16,8 @@ export const ProjectArr = [
         imgUrl: "https://shots.codepen.io/Web-Tech-DUDE/pen/rNJZaRm-512.jpg",
         repoUrl: "https://codepen.io/Web-Tech-DUDE/pen/rNJZaRm",
         url: "https://codepen.io/Web-Tech-DUDE/pen/rNJZaRm"
-    }, {
+    }, 
+    {
         id: 3,
         name: "First Ever FitX Project v1",
         description: "A landing website for gym",
@@ -23,7 +25,8 @@ export const ProjectArr = [
         imgUrl: "https://camo.githubusercontent.com/5feadd3703c34a35776c65e4ad6a573d7c17f711126a136943d1a4b42808df7a/68747470733a2f2f692e6962622e636f2f765639315350482f53637265656e2d53686f742d323032322d30342d32372d61742d32322d34352d34342e706e67",
         repoUrl: "https://github.com/webtechdude/Batch-221_2--4090975--Fatin-Noor-Taki-Assignment-8-FitX",
         url: "https://fitx-taki.netlify.app/"
-    }, {
+    }, 
+    {
         id: 4,
         name: "Social Media Referencing Site",
         description: "Links to social sites like LinkTree",
@@ -31,15 +34,17 @@ export const ProjectArr = [
         imgUrl: "https://camo.githubusercontent.com/172b766665ab8def23c27911b361710829c828c0e9f768e3da63343176d500cc/68747470733a2f2f692e6962622e636f2f547147477743782f53637265656e2d53686f742d323032322d30352d30322d61742d32332d34382d33302d66756c6c706167652e706e67",
         repoUrl: "https://github.com/webtechdude/Fatin-Noor-Taki---Profile-Links",
         url: "https://social-taki.netlify.app/"
-    }, {
-        id: 7,
-        name: "React Counter App with Dark Theme Toggler",
-        description: "My First Simple React App",
-        lang: ["scss", "react"],
-        imgUrl: "./project_img/5.png",
-        repoUrl: "https://github.com/webtechdude/React-Starter-App-With-CSS-Class",
-        url: "https://cssthemereact.netlify.app/"
-    }, {
+    },
+    {
+        id: 5,
+        name: "Calculator App",
+        description: "Modern Looking Calculator App made using Vanilla Javascript",
+        lang: ["html", "css", "js"],
+        imgUrl: "https://i.ibb.co/5W5TgP7/Screen-Shot-2022-06-05-at-01-41-22.png",
+        repoUrl: "https://github.com/webtechdude/JS-Calculator",
+        url: "https://codepen.io/Web-Tech-DUDE/pen/yLvpemd"
+    },
+    {
         id: 6,
         name: "Superhero Gallery",
         description: "Connected to API where you can search and know more about your superhero",
@@ -49,13 +54,13 @@ export const ProjectArr = [
         url: "https://cheery-choux-bda1b1.netlify.app/"
     },
     {
-        id: 1,
-        name: "FitX Final Version v2",
-        description: "Final Version of FitX with PHP",
-        lang: ["html", "css", "scss", "php", "mysql"],
-        imgUrl: "https://i.ibb.co/FmnXLxz/spelaean-mixture-000webhostapp-com-1.png",
-        repoUrl: "https://spelaean-mixture.000webhostapp.com/",
-        url: "https://spelaean-mixture.000webhostapp.com/"
+        id: 7,
+        name: "React Counter App with Dark Theme Toggler",
+        description: "My First Simple React App",
+        lang: ["scss", "react"],
+        imgUrl: "./project_img/5.png",
+        repoUrl: "https://github.com/webtechdude/React-Starter-App-With-CSS-Class",
+        url: "https://cssthemereact.netlify.app/"
     },
     {
         id: 8,
