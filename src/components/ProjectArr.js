5081dcffc540200a8955a7ce8354d0c486f6f357
Web Tech DUDE <71288016+webtechdude@users.noreply.github.com>
2024-03-20 20:32:43 +0000

    final almost

--- src/components/ProjectArr.js
+++ src/components/ProjectArr.js
@@ -22,7 +22,7 @@ export const ProjectArr = [
         name: "First Ever FitX Project v1",
         description: "A landing website for gym",
         lang: ["html", "css", "js"],
-        imgUrl: "https://camo.githubusercontent.com/5feadd3703c34a35776c65e4ad6a573d7c17f711126a136943d1a4b42808df7a/68747470733a2f2f692e6962622e636f2f765639315350482f53637265656e2d53686f742d323032322d30342d32372d61742d32322d34352d34342e706e67",
+        imgUrl: "https://raw.githubusercontent.com/webtechdude/Batch-221_2--4090975--Fatin-Noor-Taki-Assignment-8-FitX/master/img/3.jpg",
         repoUrl: "https://github.com/webtechdude/Batch-221_2--4090975--Fatin-Noor-Taki-Assignment-8-FitX",
         url: "https://fitx-taki.netlify.app/"
     }, 
@@ -31,7 +31,7 @@ export const ProjectArr = [
         name: "Social Media Referencing Site",
         description: "Links to social sites like LinkTree",
         lang: ["html", "css", "js"],
-        imgUrl: "https://camo.githubusercontent.com/172b766665ab8def23c27911b361710829c828c0e9f768e3da63343176d500cc/68747470733a2f2f692e6962622e636f2f547147477743782f53637265656e2d53686f742d323032322d30352d30322d61742d32332d34382d33302d66756c6c706167652e706e67",
+        imgUrl: "https://raw.githubusercontent.com/webtechdude/Fatin-Noor-Taki---Profile-Links/master/images/4.jpg",
         repoUrl: "https://github.com/webtechdude/Fatin-Noor-Taki---Profile-Links",
         url: "https://social-taki.netlify.app/"
     },
@@ -74,10 +74,46 @@ export const ProjectArr = [
     {
         id: 9,
         name: "MoonTube",
-        description: "Really simple YouTube homepage clone created for my friend's project",
+        description: "Really simple YouTube homepage clone created for my friend's project made within an hour from scratch",
         lang: ["html", "css", "js", "scss"],
         imgUrl: "./project_img/9.jpg",
         repoUrl: "https://github.com/webtechdude/MoonTube",
         url: "https://webtechdude.github.io/MoonTube/"
+    },
+    {
+        id: 10,
+        name: "WebTechDUDE YouTube Personal Webpage",
+        description: "One of my earliest webpages made with HTML, CSS and a little bit of JS",
+        lang: ["html", "css", "js"],
+        imgUrl: "./project_img/10.jpg",
+        repoUrl: "https://github.com/webtechdude/webpage",
+        url: "https://webtechdude.github.io/webpage/"
+    },
+    {
+        id: 11,
+        name: "Quantalazer - Practice Webpage",
+        description: "My second practice webpage about an imaginary headphone",
+        lang: ["html", "css"],
+        imgUrl: "https://shots.codepen.io/username/pen/BamevpK-512.webp",
+        repoUrl: "https://codepen.io/Web-Tech-DUDE/pen/BamevpK",
+        url: "https://cdpn.io/Web-Tech-DUDE/fullcpgrid/BamevpK"
+    },
+    {
+        id: 12,
+        name: "Seashore Lottery Form",
+        description: "One of my earliest practice web form",
+        lang: ["html", "css"],
+        imgUrl: "https://shots.codepen.io/username/pen/gOXqjga-1280.webp?version=1646404686",
+        repoUrl: "https://codepen.io/Web-Tech-DUDE/pen/gOXqjga",
+        url: "https://cdpn.io/Web-Tech-DUDE/fullcpgrid/gOXqjga"
+    },
+    {
+        id: 12,
+        name: "Tribute to - Sher-E-Bangla A. K. Fazlul Huq",
+        description: "My first single page webpage with a little description about AK Fazlul Huq",
+        lang: ["html", "css"],
+        imgUrl: "https://shots.codepen.io/username/pen/OJOBOPx-512.webp?version=1646064225",
+        repoUrl: "https://codepen.io/Web-Tech-DUDE/pen/OJOBOPx",
+        url: "https://cdpn.io/Web-Tech-DUDE/fullcpgrid/OJOBOPx"
     },
 ]
